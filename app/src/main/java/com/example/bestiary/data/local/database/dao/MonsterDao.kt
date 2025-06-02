2d2215c67b285183144013a6eb31f90acfb76440
Lev <kartoxauaua@gmail.com>
2025-06-02 14:22:49 +0700

    Добавление Dao

---
+++ app/src/main/java/com/example/bestiary/data/local/database/dao/MonsterDao.kt
@@ -0,0 +1,28 @@
+package com.example.bestiary.data.local.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.bestiary.data.local.database.entity.MonsterEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MonsterDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMonster(monster: MonsterEntity)
+
+    @Delete
+    suspend fun deleteMonster(monster: MonsterEntity)
+
+    @Query("SELECT * FROM monsters WHERE isFavorite = 1")
+    fun getFavoriteMonsters(): Flow<List<MonsterEntity>>
+
+    @Query("SELECT * FROM monsters WHERE index = :index")
+    suspend fun getMonsterByIndex(index: String): MonsterEntity?
+
+    @Update
+    suspend fun updateMonster(monster: MonsterEntity)
+}
